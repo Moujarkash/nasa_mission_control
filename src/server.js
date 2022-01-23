340c20a07aa7c9b9493267b95231cb5b1948b3a6
Mouhmmad Moujarkash <mouhmmad488@gmail.com>
2022-01-23 10:25:55 +0200

    - add env file

--- src/server.js
+++ src/server.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 const {loadPlanetsData} = require("./models/planets.model");
+const {MONGO_URL} = require('../env');
 
 const PORT = process.env.PORT || 8000;
-
-const MONGO_URL = 'mongodb+srv://nasa-api:iR9HHfyJSN8NqsV@nasacluster.yp8sy.mongodb.net/nasa?retryWrites=true&w=majority';
 
 const server = http.createServer(app);
 
