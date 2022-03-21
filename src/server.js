f1c22b848dfc61b410146885bd050e4b83595bd9
Mouhmmad Moujarkash <mouhmmad488@gmail.com>
2022-03-21 20:00:15 +0200

    - add env file and build github workflow

--- src/server.js
+++ src/server.js
@@ -1,24 +1,22 @@
 const http = require('http');
 
 const app = require('./app');
-const {mongoConnect} = require('./services/mongo');
-const {loadPlanetsData} = require("./models/planets.model");
-const {loadLaunchesData} = require('./models/launches.model');
+const { mongoConnect } = require('./services/mongo');
+const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchesData } = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-
-
 async function startServer() {
-    await mongoConnect();
-    await loadPlanetsData();
-    await loadLaunchesData();
+  await mongoConnect();
+  await loadPlanetsData();
+  await loadLaunchesData();
 
-    server.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}...`);
-    });
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
 }
 
 startServer();
